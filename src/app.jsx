d51955b8fdd597a915e479a1d7ab46866227445b
Matt Molnar <matthew.molnar@gmail.com>
2016-12-20 23:57:36 -0500

    Add CodeMirror

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,29 @@
 import styles from './index.scss';
+import __codemirrorstyle from 'codemirror/lib/codemirror.css';
+import __jsstyle from 'codemirror/mode/javascript/javascript';
+import __paraiso from 'codemirror/theme/paraiso-dark.css';
 import React from 'react';
+import CodeMirror from 'react-codemirror';
 
 export default class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {code:
+      '// Code\n'+
+      'function hi(){\n'+
+      '  console.log(\'hello\');\n'+
+      '}'
+    };
+  }
+
+  updateCode(newCode){
+    this.setState({code:newCode});
+  }
+
   render() {
+    const options = {lineNumbers:true, mode:'javascript',theme : 'paraiso-dark'};
     return (
-      <div>
-        <h1>It Works!</h1>
-        <p>This React project just works including <span className={styles.redBg}>module</span> local styles.</p>
-        <p>Enjoy!</p>
-      </div>
+      <CodeMirror value={this.state.code} onChange={this.updateCode.bind(this)} options={options} />
     )
   }
 }
